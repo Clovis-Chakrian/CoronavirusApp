94733d3b8d69f0ef6f9cffe7cca3ee73fba667b5
Clóvis Chakrian <clovischakrian26@gmail.com>
2020-06-23 00:33:59 -0300

    just updating the package files

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { View, Text, StatusBar, Image } from 'react-native';
 
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, useScrollToTop } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { AppLoading } from 'expo';
@@ -12,7 +12,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import { 
+import {
   Ubuntu_400Regular,
   Ubuntu_700Bold,
 } from '@expo-google-fonts/ubuntu';
@@ -22,6 +22,9 @@ import styles from './src/styles/style';
 import Brazil from './src/pages/Brazil';
 import World from './src/pages/World';
 import Hospitals from './src/pages/Hospitals';
+import BrazilStates from './src/pages/BrazilStates';
+import Info from './src/pages/Info';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Tab = createBottomTabNavigator();
 
@@ -33,9 +36,21 @@ const Theme = {
   },
 };
 
+function button() {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Image source={require('./assets/icons/BrazilFlag_1.png')} style={{ height: 40, width: 40, alignSelf: 'center', marginTop: 30 }} />
+      </TouchableOpacity>
+    </View>
+  )
+};
+
+
 export default function App() {
   let [fontsLoaded] = useFonts({
     Roboto_400Regular,
+    Roboto_700Bold,
     Ubuntu_400Regular,
     Ubuntu_700Bold
   });
@@ -51,10 +66,61 @@ export default function App() {
         <View style={styles.titleAppContainer}>
           <Text style={styles.titleApp}>CoronavírusApp</Text>
         </View>
-        <Tab.Navigator initialRouteName='Mundo'>
-          <Tab.Screen name='Brasil' component={Brazil} />
-          <Tab.Screen name='Mundo' component={World} />
-          <Tab.Screen name='Postos de Saúde' component={Hospitals} />
+        <Tab.Navigator
+          initialRouteName='Brasil'
+          lazy={false}
+          tabBarOptions={{
+            showLabel: false,
+            activeBackgroundColor: '#DCDCDC',
+            inactiveBackgroundColor: '#FFF',
+            tabStyle: { borderTopColor: '#00BFFF', borderTopWidth: 1 },
+          }}
+        >
+
+          <Tab.Screen
+            name='Brasil'
+            component={Brazil}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Image source={require('./assets/icons/BrazilFlag_1.png')} color={color} size={size} style={{ height: 40, width: 40, alignSelf: 'center', marginTop: 30 }} />
+              ),
+            }} />
+
+          <Tab.Screen
+            name='Mundo'
+            component={World}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Image source={require('./assets/icons/world.png')} color={color} size={size} style={{ height: 130, width: 130, alignSelf: 'center', marginTop: 26, marginRight: 20 }} />
+              ),
+            }} />
+
+          <Tab.Screen
+            name='Hospitais'
+            component={Hospitals}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Image source={require('./assets/icons/Hospitals.png')} color={color} size={size} style={{ height: 70, width: 70, alignSelf: 'center', marginTop: 45, marginLeft: 3 }} />
+              ),
+            }} />
+
+          <Tab.Screen
+            name='Estado'
+            component={BrazilStates}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Image source={require('./assets/icons/brazilmap.png')} color={color} size={size} style={{ height: 35, width: 35, alignSelf: 'center', marginTop: 20 }} />
+              ),
+            }} />
+
+          <Tab.Screen
+            name='Info'
+            component={Info}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Image source={require('./assets/icons/Info.png')} color={color} size={size} style={{ height: 50, width: 40, alignSelf: 'center', marginTop: 30 }} />
+              ),
+            }} />
         </Tab.Navigator>
       </NavigationContainer>
     );
