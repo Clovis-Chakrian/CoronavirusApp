398b1f064b0ae5e2c10dd0a4217e006915758f63
Clóvis Chakrian <clovischakrian26@gmail.com>
2020-06-23 00:27:09 -0300

    This feature show some tips against COVID19

---
+++ src/pages/Info/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ScrollView, View, Text, Image } from 'react-native';
+
+// Styles
+import styles from '../../styles/style';
+import infoStyles from '../../styles/infoStyles';
+
+// Images
+import sick from '../../../assets/img/sick.png';
+import mask from '../../../assets/img/mask.png';
+import home from '../../../assets/img/homeOffice.png';
+import diagnostic from '../../../assets/img/diagnostic.png';
+
+function TipCard(props) {
+  return (
+    <View style={infoStyles.tipCard}>
+      <Text style={infoStyles.title}>{props.title}</Text>
+      <View style={infoStyles.imgContainer}>
+        <Image source={props.img} style={infoStyles.imgs} />
+        <Text style={infoStyles.tip}>{props.tip}</Text>
+      </View>
+    </View>
+  );
+};
+
+const Info = () => {
+  return (
+    <ScrollView style={styles.container}>
+      <TipCard title={'Casa...'} img={home} tip={'FIQUE EM CASA, sempre que possível, faça o máximo para não sair dela e assim evitar ser contaminado e/ou propagar o COVID-19.'} />
+      <TipCard title={'Máscara...'} img={mask} tip={'SEMPRE que haja necessidade de sair de casa, a use. Pense nela como seu celular, sempre que tiver que sair de casa leve-a, é um item extremamente necessário nessa luta contra o vírus.'} />
+      <TipCard title={'Sentindo-se com um dos sintomas...'} img={sick} tip={'Caso sinta algum dos sintomas de COVID-19, procure a unidade de saúde mais próxima.'} />
+      <TipCard title={'Diagnosticado, e agora?...'} img={diagnostic} tip={'Agora faça tudo o que seu médico recomendar, fique sempre em casa, caso more com mais pessoas, mantenha distância.'} />
+    </ScrollView>
+  );
+};
+
+export default Info;
