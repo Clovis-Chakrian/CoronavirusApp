f846582f396d00881d480abcd0d8c1081fc3d448
Clóvis Chakrian <clovischakrian26@gmail.com>
2020-06-22 23:46:11 -0300

    integrated the API with all data needed

--- src/pages/Brazil/index.js
+++ src/pages/Brazil/index.js
@@ -1,44 +1,72 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 
 import styles from '../../styles/style';
 import cardStyles from '../../styles/cardsStyle';
 
-const Brazil = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.cardsContainer}>
-        <View style={cardStyles.deathsCard}>
-          <Text style={cardStyles.deathsLabel}>Mortes Brasil:</Text>
-          <Text style={cardStyles.deathsNumber}>0</Text>
-        </View>
+import api from '../../services/api';
 
-        <View style={cardStyles.midleCardsContainer}>
-          <View style={cardStyles.casesMediumCard}>
-            <Text style={cardStyles.casesMediumLabel}>Casos Brasil:</Text>
+export default class Main extends Component {
 
-            <Text style={cardStyles.casesNumber}>0</Text>
+  componentDidMount() {
+    this.loadCases();
+  }
+
+  state = {
+    // Brazil data.
+    casesB: '...',
+    deathsB: '...',
+    lethalityB: '...',
+    recoveredB: '...',
+  };
+
+  loadCases = async () => {
+    // Brazil update.
+    const responseB = await api.get("/brazil");
+
+    const brazil = responseB.data;
+
+    const lethalityB = ((brazil.deaths / brazil.cases) * 100).toFixed(1).replace('.', ',') + '%';
+
+    this.setState({ casesB: brazil.cases });
+    this.setState({ deathsB: brazil.deaths });
+    this.setState({ lethalityB: lethalityB });
+    this.setState({ recoveredB: brazil.recovered });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.cardsContainer}>
+          <View style={cardStyles.deathsCard}>
+            <Text style={cardStyles.deathsLabel}>Mortes Brasil:</Text>
+            <Text style={cardStyles.deathsNumber}>{this.state.deathsB}</Text>
           </View>
 
-          <View style={cardStyles.lethalityCard}>
-            <Text style={cardStyles.lethalityLabel}>Letalidade Brasil:</Text>
-            <Text style={cardStyles.lethalityNumber}>0%</Text>
+          <View style={cardStyles.midleCardsContainer}>
+            <View style={cardStyles.casesMediumCard}>
+              <Text style={cardStyles.casesMediumLabel}>Casos Brasil:</Text>
+              <Text style={cardStyles.casesNumber}>{this.state.casesB}</Text>
+            </View>
+
+            <View style={cardStyles.lethalityCard}>
+              <Text style={cardStyles.lethalityLabel}>Letalidade Brasil:</Text>
+              <Text style={cardStyles.lethalityNumber}>{this.state.lethalityB}</Text>
+            </View>
+          </View>
+
+          <View style={cardStyles.recoveredCard}>
+            <Text style={cardStyles.recoveredLabel}>Recuperados Brasil:</Text>
+            <Text style={cardStyles.recoveredNumber}>{this.state.recoveredB}</Text>
           </View>
         </View>
 
-        <View style={cardStyles.recoveredCard}>
-          <Text style={cardStyles.recoveredLabel}>Recuperados Brasil:</Text>
-          <Text style={cardStyles.recoveredNumber}>0</Text>
+        <View style={styles.imageContainer}>
+          <Image source={require('../../../assets/img/mask.png')} style={styles.images} />
+          <Text style={styles.imageLabel}>Sempre use sua máscara ao sair.</Text>
         </View>
       </View>
-
-      <View style={styles.imageContainer}>
-        <Image source={require('../../assets/mask.jpg')} style={styles.images} />
-        <Text style={styles.imageLabel}>Sempre use sua máscara ao sair.</Text>
-      </View>
-    </View>
-  );
-};
-
-export default Brazil;
+    );
+  }
+}
